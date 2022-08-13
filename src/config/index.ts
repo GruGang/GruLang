4386c27f508af84f6c040d70e9e9d3849e8af472
Nate Emerson <emerson.nate@gmail.com>
2022-08-13 02:10:53 -0700

    First pass at basic CLI stuff and parsing.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,73 @@
-
-type gruKeyword = "finna" | "nocap" | "cap" | "oof" | "yikes" | "fr" | "yeet" | "smash" | "fresh" | "fr" | "lowkey" | "fr lowkey" | "ded" | "bet" | "Gotchu()" | "sheesh" | "bussin" | "bless" | "sus" | "ongod" | "holUp()" | "kms" | "slide.dm()" | "Fam" | "Chad" | "Gigachad"
+export type gruKeyword =
+  | "finna"
+  | "nocap"
+  | "cap"
+  | "oof"
+  | "yikes"
+  | "facts"
+  | "yeet"
+  | "be"
+  | "===="
+  | "W"
+  | "L"
+  | "smash"
+  | "fresh"
+  | "fr"
+  | "lowkey"
+  | "fr lowkey"
+  | "ded"
+  | "bet"
+  | "Gotchu"
+  | "sheesh"
+  | "bussin"
+  | "bless"
+  | "sus"
+  | "ong"
+  | "holUp"
+  | "Fam"
+  | "Chad"
+  | "Gigachad"
+  | ":hash:"
+  | ":pencil2:";
 
 export const grumap = new Map<string, gruKeyword>([
+  // Main keywords
   ["function", "finna"],
   ["true", "nocap"],
   ["false", "cap"],
   ["error", "oof"],
   ["throw", "yikes"],
-  ["const", "fr"],
+  ["const", "facts"],
   ["return", "yeet"],
   ["delete", "smash"],
-  ["function", "fr"],
+  ["new", "fresh"],
+
+  // Flow Control
+  ["if", "fr"],
+  ["else", "lowkey"],
+  ["else if", "fr lowkey"],
+  ["break", "ded"],
+  ["try", "bet"],
+  ["=", "be"],
+  ["===", "===="],
+  ["?", "W"],
+  [",", "L"],
+
+  // Promises
+  ["Promise", "Gotchu"],
+  ["reject", "sheesh"],
+  ["resolve", "bussin"],
+  ["then", "bless"],
+  ["catch", "sus"],
+  ["finally", "ong"],
+
+  // Types
+  ["number", ":hash:"],
+  ["string", ":pencil2:"],
+
+  // Other shit that I can't be fucked to categorise
+  ["setTimeout", "holUp"],
+  ["Array", "Fam"],
+  ["Object", "Chad"],
+  ["Map", "Gigachad"],
 ]);
---
+++ src/gruc.ts
@@ -0,0 +1,40 @@
+import { exit } from "process";
+import { gruKeyword, grumap } from "./config";
+import { Command } from "commander";
+
+const VERSION_NUMBER = "0.0.1";
+
+const parseGru: (gruCode: string) => string = (gruCode: string) => {
+  let tsCode = gruCode.replace("finna", "function");
+  for (let entry of grumap.entries()) {
+    tsCode = tsCode.replace(entry[1], entry[0]);
+  }
+  return tsCode;
+};
+
+const fs = require("fs-extra");
+const program = new Command().name("GRU");
+
+const word: gruKeyword = "finna";
+const translatedWord = grumap.get(word);
+console.log(translatedWord);
+
+program
+  .description("Tryna parse Gru?")
+  .argument("[sourceFile]", "The source .gru file")
+  .option("--help")
+  .version(VERSION_NUMBER, "-v, --version", "Display the version number")
+  .parse(process.argv);
+
+program.parse();
+
+const args = program.args;
+if (!args[0]) {
+  console.log("ERROR: Please provide input file");
+  process.exit(1);
+}
+const sourceCode: string = fs.readFileSync(args[0]).toString();
+const parsedCode: string = parseGru(sourceCode);
+process.stdout.write(parsedCode);
+
+exit(0);
